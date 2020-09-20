484aaacc0e0f0b68b7dc5b852e734cb37f92420e
amireslamdoust <amireslamdoust@gmail.com>
2020-09-20 05:48:03 +0200

    remove extra test

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -3,10 +3,5 @@ describe('test convert works', () => {
     cy.visit('/')
     cy.get('[data-cy="numeric-input-number"]').type('215')
     cy.get('[data-cy="roman-input-roman"]').should('have.value', 'CCXV')
-
-    cy.wait(400)
-
-    cy.get('[data-cy="roman-input-roman"]').type('MCX')
-    cy.get('[data-cy="numeric-input-number"]').should('have.value', '1,110')
   })
 })
